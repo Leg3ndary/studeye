6cd5e87a12cdf4717718f618b5a7cece48424596
Leg3ndary <ben.zhou.2006@gmail.com>
2023-09-16 15:05:38 -0400

    Pushing notes new stuff

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,16 +4,17 @@ import { useState } from "react";
 const ubuntu = Ubuntu({ subsets: ["latin"], weight: ["400", "700"] });
 
 export default function Home() {
-    const [notes, setNotes] = useState(["Your **notes** will go here!"]);
+    const [notes, setNotes] = useState("Your **notes** will go **here**!");
 
     function formatText(text) {
         const regex = /\*\*(.*?)\*\*/g;
+
         return text.split(regex).map((part, index) => {
             if (index % 2 === 0) {
                 return part;
             } else {
                 return (
-                    <span key={index} className="font-black drop-shadow-glowbL">
+                    <span key={index} className="font-black drop-shadow-glow">
                         {part}
                     </span>
                 );
@@ -21,25 +22,28 @@ export default function Home() {
         });
     }
 
+    const handleContentChange = (e) => {
+        const newContent = e.target.value;
+        setNotes(newContent);
+    };
+
     return (
         <main
             className={`flex min-h-screen flex-col items-center justify-between ${ubuntu.className}`}
         >
+            <h1 className="p-8 text-6xl font-bold text-center drop-shadow-glow">
+                Studeye
+            </h1>
             <div className="w-[950px] bg-htn">
-                <h1 className="p-8 text-6xl font-bold text-center drop-shadow-glow">
-                    Studeye
-                </h1>
-                <div
-                    className="w-full h-auto grid-cols-2 gap-24 text-3xl text-white"
-                    contentEditable="true"
-                    onInput={(e) => {
-                        const newNotes = [...notes];
-                        newNotes[0] = e.target.innerHTML;
-                        setNotes(newNotes);
-                    }}
-                >
-                    {formatText(notes[0])}
+                
+                <div className="w-full h-screen p-4 text-3xl text-white">
+                    {formatText(notes)}
                 </div>
+                <textarea
+                    className="w-full h-screen grid-cols-2 gap-24 p-4 text-3xl text-white bg-inherit"
+                    value={notes}
+                    onChange={handleContentChange}
+                />
             </div>
         </main>
     );
