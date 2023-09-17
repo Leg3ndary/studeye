80b4e550e5c96bea5c2bbcfec9aec2690d15af7d
Leg3ndary <ben.zhou.2006@gmail.com>
2023-09-16 20:41:22 -0400

    Works with points

--- eye/stt.py
+++ eye/stt.py
@@ -1,13 +1,15 @@
 import pytesseract
 from PIL import ImageGrab
 
-def get_text(point: int) -> str:
+def get_text(x: int, y: int) -> str:
     """
     Read an image and use pytesseract to return the text in the image.
     """
-    bbox = (81, 135, 500, 300)
+    # Left, upper, right, lower
+    bbox = (x - 250, y - 100, x + 250, y + 100)
 
     img = ImageGrab.grab(bbox)
+    img.show()
 
     text = pytesseract.pytesseract.image_to_string(img)
 
